10c0d6c43deb1eb2e06fbf51d7f5448de57f9974
krustew17 <krasimircho555@gmail.com>
2024-11-11 12:49:27 +0200

    setup sequelize database orm

--- server/app.js
+++ server/app.js
@@ -2,22 +2,25 @@ import * as dotenv from "dotenv";
 import express from "express";
 
 import config from "./config/index.js";
-import setupMySql from "./loaders/mysql.js";
+import db from "./models/index.js";
 
 dotenv.config();
 
 async function startServer() {
   const app = express();
 
-  await setupMySql({ expressApp: app });
+  db.sequelize
+    .sync()
+    .then(() => {
+      console.log("Tables have been synchronized successfully.");
+    })
+    .catch((err) => {
+      console.log("Something went wrong synchronizing tables.");
+    });
 
   app
     .listen(config.port, () => {
-      console.log(
-        `**********************************************
-      Server running on port ${config.port}
-      **********************************************`,
-      );
+      console.log(`Server running on port ${config.port}`);
     })
     .on("error", (err) => {
       console.error(err);
---
+++ server/models/userModel.js
@@ -0,0 +1,37 @@
+export default (sequelize, Sequelize) => {
+  const User = sequelize.define("user", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastLogin: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+  });
+};
