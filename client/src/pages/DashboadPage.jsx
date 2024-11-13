dccfcc99780e9ca9296c9737a5e5c8e7d667e09d
krustew17 <krasimircho555@gmail.com>
2024-11-14 01:50:50 +0200

    implemented 2 cards for the dashboard - active users and logins count

--- client/src/pages/DashboadPage.jsx
+++ client/src/pages/DashboadPage.jsx
@@ -1,11 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "../components/Navbar";
+import apiEndpoints from "../config/apiEndpoints";
+import requester from "../common/requester";
+import Chart from "../components/charts/Chart";
 
 const DashboardPage = () => {
+    const [activeUsersCount, setActiveUsersCount] = useState(0);
+    const [loginsCount, setLoginsCount] = useState(0);
+
+    const fetchActiveUsers = async () => {
+        try {
+            const { responseJson } = await requester(
+                apiEndpoints.metrics.activeUsers.url,
+                {
+                    method: apiEndpoints.metrics.activeUsers.method,
+                }
+            );
+            setActiveUsersCount(responseJson.activeUsersCount);
+        } catch (error) {
+            console.error("Error fetching active users:", error);
+        }
+    };
+
+    const fetchLoginsCount = async () => {
+        try {
+            const { responseJson } = await requester(
+                `${apiEndpoints.metrics.loginsCount.url}?timeframe=1m`,
+                {
+                    method: apiEndpoints.metrics.loginsCount.method,
+                }
+            );
+            setLoginsCount(responseJson.loginsCount);
+        } catch (error) {
+            console.error("Error fetching logins count:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchLoginsCount();
+        fetchActiveUsers();
+    }, []);
+
     return (
         <div>
             <Nav />
-            <h1>Dashboard</h1>
+            <div className="flex flex-1 mt-2 px-4 gap-2 w-full">
+                <div className="bg-stone-800 rounded-xl max-w-[250px] w-full p-4">
+                    <h1 className="text-2xl">Active Users</h1>
+                    <div className="flex">
+                        <h1 className="text-4xl">{activeUsersCount}</h1>
+                    </div>
+                </div>
+                <div className="bg-stone-800 rounded-xl p-4 max-w-[300px] w-full">
+                    <div className="flex justify-between">
+                        <h1 className="text-2xl">Monthly Logins</h1>
+                        <button>...</button>
+                    </div>
+                    <div className="flex">
+                        <h1 className="text-4xl">{loginsCount}</h1>
+                    </div>
+                </div>
+                <div>
+                    <Chart />
+                </div>
+            </div>
         </div>
     );
 };
--- server/api/services/validations/validTimeframe.js
+++ server/api/services/validations/validTimeframe.js
@@ -1,10 +1,6 @@
 import validTimeFrames from "../../../config/timeFrames.js";
 
 const validateTimeframe = (timeframe) => {
-    if (!timeframe) {
-        throw new Error("timeframe is required.");
-    }
-
     if (!validTimeFrames[timeframe]) {
         throw new Error("invalid timeframe.");
     }
