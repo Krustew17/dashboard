dc2d1fad2e88774a78c8a6d7d7c9ae41584b4498
krustew17 <krasimircho555@gmail.com>
2024-11-18 18:04:03 +0200

    updated server config again

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const envFound = dotenv.config();
-if (envFound.error && process.env.NODE_ENV !== "development") {
+if (envFound.error && process.env.NODE_ENV !== "production") {
     // This error should crash whole process
 
     throw new Error("⚠️  Couldn't find .env file  ⚠️");
