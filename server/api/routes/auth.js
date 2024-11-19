1babe592e8da69c895296c87b4202c626c81580f
krustew17 <krasimircho555@gmail.com>
2024-11-19 15:39:59 +0200

    test workflow and create changepassword endpoint

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
-import { loginUser, registerUser } from "../controllers/auth.controller.js";
+import {
+    changePassword,
+    loginUser,
+    registerUser,
+} from "../controllers/auth.controller.js";
 import middlewares from "../middleware/index.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", middlewares.checkUserDevice, loginUser);
+router.post("/change-password", changePassword);
 
 export default router;
